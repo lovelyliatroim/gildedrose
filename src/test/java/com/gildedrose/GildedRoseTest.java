73c60b155e76ea3d062435594c8ff8c72dc7e7b0
David Leitner <david@leitner.io>
2017-12-04 15:23:31 +0100

    Add tests for special Items (Aged-Brie, Sulfuras, Backstagepasses)

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,9 +7,20 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     private static final String DEFAULT_ITEM_NAME = "foo";
-    private static final int DEFAULT_SELLIN = 10;
+
     private static final int DEFAULT_QUALITY = 10;
-    private static final int DEFAULT_PASSED_SELLIN = -1;
+    private static final int QUALITY_UPPERLIMIT = 50;
+
+    private static final String ITEM_AGEDBRIE = "Aged Brie";
+    private static final String ITEM_SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String ITEM_BACKSTAGEPASSES = "Backstage passes to a TAFKAL80ETC concert";
+
+    private static final int DEFAULT_SELLIN = 10;
+    private static final int SELLIN_PASSED = -1;
+    private static final int SELLIN_ZERO = 0;
+    private static final int SELLIN_LESSTHAN5 = 3;
+    private static final int SELLIN_OVER5LESS10 = 7;
+    private static final int SELLIN_OVER10 = 11;
 
     @Test
     public void updateValues_shouldNotChangeTheNameOfAnItem() {
@@ -28,7 +39,7 @@ public class GildedRoseTest {
 
         app.updateValues();
 
-        assertEquals(DEFAULT_QUALITY-1, app.items[0].quality);
+        assertEquals(DEFAULT_QUALITY - 1, app.items[0].quality);
     }
 
     @Test
@@ -38,17 +49,113 @@ public class GildedRoseTest {
 
         app.updateValues();
 
-        assertEquals(DEFAULT_SELLIN-1, app.items[0].sellIn);
+        assertEquals(DEFAULT_SELLIN - 1, app.items[0].sellIn);
     }
 
     @Test
     public void updateValues_givenAnItemWithPassedSellIn_thenQualityDecreasesTwiceAsFast() {
-        Item[] items = new Item[]{new TestItem().withSellIn(DEFAULT_PASSED_SELLIN).withQuality(3).toItem()};
+        Item[] items = new Item[]{new TestItem().withSellIn(SELLIN_PASSED).withQuality(3).toItem()};
         GildedRose app = new GildedRose(items);
 
         app.updateValues();
 
         assertEquals(1, app.items[0].quality);
+    }
+
+    @Test
+    public void updateValues_shouldNotResultInANegativeQuality() {
+        Item[] items = {new TestItem().withQuality(0).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void updateValues_givenAgedBree_shouldIncreaseItsQualityOverTime() {
+        Item[] items = {new TestItem().withQuality(DEFAULT_QUALITY).withName(ITEM_AGEDBRIE).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        int expectedIncreasedQuality = DEFAULT_QUALITY + 1;
+        assertEquals(expectedIncreasedQuality, app.items[0].quality);
+    }
+
+    @Test
+    public void updateValues_givenAgedBreeWithPassedSellIn_shouldIncreaseItsQualityByTwo() {
+        Item[] items = {new TestItem().withQuality(DEFAULT_QUALITY).withSellIn(SELLIN_PASSED).withName(ITEM_AGEDBRIE).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        int expectedQualityIncreasedByTwo = DEFAULT_QUALITY + 2;
+        assertEquals(expectedQualityIncreasedByTwo, app.items[0].quality);
+    }
+
+    @Test
+    public void updateValues_givenAgedBrie_shouldNotBreachOver50() {
+        Item[] items = {new TestItem().withQuality(QUALITY_UPPERLIMIT).withName(ITEM_AGEDBRIE).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        assertEquals(QUALITY_UPPERLIMIT, app.items[0].quality);
+    }
+
+    @Test
+    public void updateValues_shouldNotHaveAnEffectOnTheQualityAndTheSellInOfSulfuras() {
+        Item[] items = {new TestItem().withQuality(DEFAULT_QUALITY).withSellIn(DEFAULT_SELLIN).withName(ITEM_SULFURAS).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        assertEquals(DEFAULT_QUALITY, app.items[0].quality);
+        assertEquals(DEFAULT_SELLIN, app.items[0].sellIn);
+    }
+
+    @Test
+    public void updateValues_givenBackstagePassesWithSellInOver10_shouldIncreaseQualityByOne() {
+        Item[] items = {new TestItem().withQuality(DEFAULT_QUALITY).withSellIn(SELLIN_OVER10).withName(ITEM_BACKSTAGEPASSES).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        int expectedQualityDecreasedByOne = DEFAULT_QUALITY + 1;
+        assertEquals(expectedQualityDecreasedByOne, app.items[0].quality);
+    }
+
+    @Test
+    public void updateValues_givenBackstagePassesWithSellInOver5Less10_shouldIncreaseQualityByTwo() {
+        Item[] items = {new TestItem().withQuality(DEFAULT_QUALITY).withSellIn(SELLIN_OVER5LESS10).withName(ITEM_BACKSTAGEPASSES).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        int expectedQualityDecreasedByTwo = DEFAULT_QUALITY + 2;
+        assertEquals(expectedQualityDecreasedByTwo, app.items[0].quality);
+    }
+
+    @Test
+    public void updateValues_givenBackstagePassesWithSellInLess5_shouldIncreaseQualityByThree() {
+        Item[] items = {new TestItem().withQuality(DEFAULT_QUALITY).withSellIn(SELLIN_LESSTHAN5).withName(ITEM_BACKSTAGEPASSES).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        int expectedQualityDecreasedByThree = DEFAULT_QUALITY + 3;
+        assertEquals(expectedQualityDecreasedByThree, app.items[0].quality);
+    }
+
+    @Test
+    public void updateValues_givenBackstagePassesWithSellOf0_shouldDropQualityTo0() {
+        Item[] items = {new TestItem().withSellIn(SELLIN_ZERO).withName(ITEM_BACKSTAGEPASSES).toItem()};
+        GildedRose app = new GildedRose(items);
+
+        app.updateValues();
+
+        assertEquals(0, app.items[0].quality);
     }
 
     static class TestItem {
