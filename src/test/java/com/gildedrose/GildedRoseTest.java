bc80ed75a38515e35495ec649e9d267d773ee027
JohnnyB <john.baxter@erstegroup.com>
2017-12-12 17:26:54 +0100

    Add sellIn should decrease for generic items test case.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,16 +10,25 @@ public class GildedRoseTest {
     private final int qualityDecreaseSpeed = 1;
     private final String genericItemName = "genericItemName";
     private final int positiveSellIn = 40;
-    private final int anyQuality = 30;
+    private final int quality = 30;
+    private final int sellInDecreaseSpeed = 1;
 
     @Test
-    public void givenAnItemWithPositiveSellInAndPositiveQualityThenTheQualityShouldDecrease() {
-        Item item = new Item(genericItemName, positiveSellIn, anyQuality);
+    public void givenAGenericItemWithPositiveSellInThenTheQualityShouldDecrease() {
+        Item item = new Item(genericItemName, positiveSellIn, quality);
 
         new GildedRose(new Item[] { item }).updateQuality();
 
-        assertEquals(anyQuality - qualityDecreaseSpeed, item.quality);
+        assertEquals(quality - qualityDecreaseSpeed, item.quality);
     }
 
+    @Test
+    public void givenAGenericItemWithPositiveSellInThenSellInShouldDecrease() {
+        Item item = new Item(genericItemName, positiveSellIn, quality);
+
+        new GildedRose(new Item[] { item }).updateQuality();
+
+        assertEquals(positiveSellIn - sellInDecreaseSpeed, item.sellIn);
+    }
 
 }
