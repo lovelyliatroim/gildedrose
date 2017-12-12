06eb601b4d58ed8562f4116d0d02db8ac8ca4e44
Christoph Pickl <christoph.pickl@erstegroup.com>
2017-12-12 17:06:27 +0100

    add test for generic item decreases quality

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,23 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
+    private final int qualityDecreaseSpeed = 1;
+
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void givenAnItemWithPositiveSellInAndPositiveQualityThenTheQualityShouldDecrease() {
+        final int quality = 30;
+        final int sellIn = 40;
+        final Item item = new Item("foo", sellIn, quality);
+
+        new GildedRose(new Item[] { item }).updateQuality();
+
+        assertEquals(quality - qualityDecreaseSpeed, item.quality);
     }
+
 
 }
