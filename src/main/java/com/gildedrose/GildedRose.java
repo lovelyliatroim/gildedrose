35fda71466f9c843701ddab92ca19f39d52f366f
Mircea Amarascu <mircea.amarascu@gmail.com>
2017-12-12 15:16:07 +0200

    Refatored GildedRose update quantity logic so that each item type is handled separately

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -28,53 +28,41 @@ class GildedRose {
 
             if (this.isSulfuras(item)) {
                 continue;
-            }
-
-            if (!this.isAgedBrie(item) && !this.isBackStagePass(item)) {
-                if (item.quality > 0) {
-                    if (!this.isSulfuras(item)) {
-                        item.quality = item.quality - 1;
-                    }
+            } else if (this.isAgedBrie(item)) {
+                if (item.quality < 50) {
+                    item.quality++;
                 }
-            } else {
+                item.sellIn--;
+                if (item.sellIn < 0 && item.quality < 50 ) {
+                    item.quality++;
+                }
+            } else if (this.isBackStagePass(item)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (this.isBackStagePass(item)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
-            }
 
-            if (!this.isSulfuras(item)) {
                 item.sellIn--;
-            }
-
-            if (item.sellIn < 0) {
-                if (!this.isAgedBrie(item)) {
-                    if (!this.isBackStagePass(item)) {
-                        if (item.quality > 0) {
-                            if (! this.isSulfuras(item)) {
-                                item.quality--;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+                item.sellIn--;
+                if (item.sellIn < 0 && item.quality > 0) {
+                    item.quality--;
                 }
             }
         }
