e042f03b72116a501340bdc948a0620b914b5263
Christoph Pickl <christoph.pickl@erstegroup.com>
2017-12-12 17:35:47 +0100

    test for degredation of quality twice as fast

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,10 +7,13 @@ import static org.junit.Assert.assertEquals;
 @SuppressWarnings({ "FieldCanBeLocal", "LocalCanBeFinal" })
 public class GildedRoseTest {
 
-    private static final int QUALITY_DECREASE_SPEED = 1;
     private static final String GENERIC_NAME = "GENERIC_NAME";
-    private static final int POSITIVE_SELL_IN = 40;
     private static final int QUALITY = 30;
+    private static final int POSITIVE_SELL_IN = 40;
+    private static final int NEGATIVE_SELL_IN = -40;
+
+    private static final int QUALITY_DECREASE_SPEED = 1;
+    private static final int DOUBLE_QUALITY_DECREASE_SPEED = QUALITY_DECREASE_SPEED * 2;
     private static final int SELL_IN_DECREASE_SPEED = 1;
 
     @Test
@@ -29,6 +32,15 @@ public class GildedRoseTest {
         updateItem(item);
 
         assertEquals(POSITIVE_SELL_IN - SELL_IN_DECREASE_SPEED, item.sellIn);
+    }
+
+    @Test
+    public void givenAGenericItemWithNegativeSellInThenTheQualityShouldDecreaseTwiceAsFast() {
+        Item item = new Item(GENERIC_NAME, NEGATIVE_SELL_IN, QUALITY);
+
+        updateItem(item);
+
+        assertEquals(QUALITY - DOUBLE_QUALITY_DECREASE_SPEED, item.quality);
     }
 
     private void updateItem(Item item) {
